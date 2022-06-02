de90a57bb2298c4099a09ae3788dcd139b42c48b
Francisco <francisco.nunez.d@usach.cl>
2022-06-01 20:34:38 -0400

    fix: done() function inside .then response

--- spec/models/categorySpec.js
+++ spec/models/categorySpec.js
@@ -9,28 +9,25 @@ const base_url = "http://164.92.96.206:8081/api/category"
 
 describe("Category Tests", function () {
     // Category: Accepted
-    it("Category: Post Category", (done) => {
-        // defining uid variable
-        let uid;
+    it("Category: Post Category", ( done ) => {
         // send POST request
-        axios.post(base_url, {
+        axios.post( base_url, {
             "name": "test", 
             "petition": 5,
         })
-        .then(function (response) {
-            uid = response.data.uid; 
+        .then( ( response ) => {
+            console.info("entre")
             // Testing
-            expect(response.data.name).toEqual("test");
-            expect(response.data.petition).toEqual(5);
-            expect(response.data.state).toEqual(true);
-
+            expect( response.data.name ).toEqual( "test" );
+            expect( response.data.petition ).toEqual( 5 );
+            expect( response.data.state ).toEqual( true );
+            // Delete from database
+            Category.findByIdAndDelete( response.data.uid );
+            done();
         })
         .catch(function (error) {
             console.info(error);
         });
-        // Delete from database
-        Category.findByIdAndDelete( uid );
-        done();
     })
     // Category: Rejected
     it( 'Category: Rejected', () => {
